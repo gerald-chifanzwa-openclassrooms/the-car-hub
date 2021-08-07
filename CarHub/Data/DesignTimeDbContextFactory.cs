2a03716d7efda6f1d8ece6c962ff8af6c34c9527
Gerald Chifanzwa <gchifianzwa@gmail.com>
2021-08-07 10:52:42 +0200

    Added migrations

---
+++ CarHub/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CarHub.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string [] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer();
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
--- CarHub/Program.cs
+++ CarHub/Program.cs
@@ -1,26 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace CarHub
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private const string LogTemplate = "[{Timestamp:HH:mm:ss, MMM dd yyyy} {Level:u3}]-{SourceContext}{NewLine}{Message:lj} {Exception}{NewLine}";
+        public static void Main(string [] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(string [] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseSerilog((context, config) =>
+                    {
+                        config
+                           .ReadFrom
+                           .Configuration(context.Configuration, "Logging")
+                           .Enrich.FromLogContext()
+                           .WriteTo
+                           .Console(theme: AnsiConsoleTheme.Code, outputTemplate: LogTemplate);
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
     }
+
 }
